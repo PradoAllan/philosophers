c268cf965a744073ffef0c60545f88ccdf10f66c
PradoAllan <allanprado.it@gmail.com>
2024-07-15 15:52:04 -0300

    up: headerfile funcs

--- libs/philo.h
+++ libs/philo.h
@@ -6,7 +6,7 @@
 /*   By: aprado <aprado@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 18:42:12 by aprado            #+#    #+#             */
-/*   Updated: 2024/07/12 18:44:05 by aprado           ###   ########.fr       */
+/*   Updated: 2024/07/15 15:49:55 by aprado           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,31 @@
 # define PHILO_H
 
 # include <stdio.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
 
+typedef struct	s_main
+{
+	int	*arr;
+}		t_main;
+
+enum	e_errors
+{
+	NO_PARAMS = -1,
+	BAD_PARAMS = -2,
+	WRONG_PARAMS = -3
+};
+
+/*----- Validations -----*/
+int	validate_input(int ac, char **av, t_main *bag);
+int	check_param(char *s);
+
+/*----- Utils -----*/
+long	ft_atol(const char *nptr);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_puterror(int error);
 
 #endif

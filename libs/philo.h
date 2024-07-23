eda2c7f86db73d7f0b2c62bf7a7ae888703b6f1b
PradoAllan <allanprado.it@gmail.com>
2024-07-23 08:44:36 -0300

    add: linked list func prototypes

--- libs/philo.h
+++ libs/philo.h
@@ -6,7 +6,7 @@
 /*   By: aprado <aprado@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 18:42:12 by aprado            #+#    #+#             */
-/*   Updated: 2024/07/16 09:47:08 by aprado           ###   ########.fr       */
+/*   Updated: 2024/07/23 08:38:30 by aprado           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ typedef struct	s_main
 {
 	int	*arr;
 	int	arr_size;
+	int	*forks;
+	int	q_forks;
 	t_philo	*head;
 }		t_main;
 
@@ -48,6 +50,13 @@ enum	e_errors
 /*----- Validations -----*/
 int	validate_input(int ac, char **av, t_main *bag);
 int	check_param(char *s);
+
+/*----- Circle Linked list -----*/
+
+void		print_philos(t_main *bag);
+void		populate_philo(t_philo **node);
+t_philo		*create_philo(int id);
+void		create_list(t_main *bag);
 
 /*----- Utils -----*/
 long	ft_atol(const char *nptr);

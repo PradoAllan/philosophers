7bbbbb27d96cb423e8c6708aabbd5a5f83e0d506
Allan <allanprado.it@gmail.com>
2024-08-21 11:10:12 -0300

    norm header file

--- libs/philo.h
+++ libs/philo.h
@@ -6,7 +6,7 @@
 /*   By: aprado <aprado@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 18:42:12 by aprado            #+#    #+#             */
-/*   Updated: 2024/08/21 10:16:01 by aprado           ###   ########.fr       */
+/*   Updated: 2024/08/21 11:04:33 by aprado           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,57 +26,57 @@
 # define MSG_THINK "is thinking"
 # define MSG_DIE "died"
 
-typedef struct		s_main	t_main;
+typedef struct s_main	t_main;
 
-typedef struct		s_arbitrator
+typedef struct s_arbitrator
 {
-	int		philo_id;
-	int		*ref_forks;
-	pthread_mutex_t	mutex;
-}			t_arbitrator;
+	int					philo_id;
+	int					*ref_forks;
+	pthread_mutex_t		mutex;
+}						t_arbitrator;
 
-typedef struct		s_fork
+typedef struct s_fork
 {
 	pthread_mutex_t	fork;
-	int		fork_status; // 1=livre 0=ocupado
-	int		fork_id;
-}			t_fork;
+	int				fork_status;
+	int				fork_id;
+}					t_fork;
 
-typedef struct		s_philo
+typedef struct s_philo
 {
-	pthread_t	tid;
-	int		id;
-	int		state;
-	long		time_to_die;
-	long		time_to_sleep;
-	long		time_to_eat;
-	int		times_eaten;
-	int		meals_counter;
-	long		last_meal_time;
-	long		dying_at;
-	t_fork		*left_fork;
-	t_fork		*right_fork;
-	t_main		*bag;
-	pthread_mutex_t	dying_mtx;
-	pthread_mutex_t	state_mtx;
-	pthread_mutex_t	meal_mtx;
-	pthread_mutex_t	last_meal_mtx;
-	pthread_mutex_t	print_mtx;
-	struct	s_philo	*next;
-	struct	s_philo	*prev;
-}			t_philo;
+	pthread_t			tid;
+	int					id;
+	int					state;
+	long				time_to_die;
+	long				time_to_sleep;
+	long				time_to_eat;
+	int					times_eaten;
+	int					meals_counter;
+	long				last_meal_time;
+	long				dying_at;
+	t_fork				*left_fork;
+	t_fork				*right_fork;
+	t_main				*bag;
+	pthread_mutex_t		dying_mtx;
+	pthread_mutex_t		state_mtx;
+	pthread_mutex_t		meal_mtx;
+	pthread_mutex_t		last_meal_mtx;
+	pthread_mutex_t		print_mtx;
+	struct s_philo		*next;
+	struct s_philo		*prev;
+}						t_philo;
 
-struct			s_main
+struct s_main
 {
-	int		*arr;
-	int		arr_size;
-	int		end_dinner;
-	int		all_ready;
-	long		start_timestamp;
-	pthread_mutex_t	end_mutex;
-	t_fork		*forks;
-	t_philo		*head;
-	t_philo		*tail;
+	int					*arr;
+	int					arr_size;
+	int					end_dinner;
+	int					all_ready;
+	long				start_timestamp;
+	pthread_mutex_t		end_mutex;
+	t_fork				*forks;
+	t_philo				*head;
+	t_philo				*tail;
 };
 
 enum	e_errors
@@ -96,8 +96,8 @@ enum	e_state
 };
 
 /*----- Validations -----*/
-int	validate_input(int ac, char **av, t_main *bag);
-int	check_param(char *s);
+int			validate_input(int ac, char **av, t_main *bag);
+int			check_param(char *s);
 
 /*----- Circle Linked list -----*/
 void		print_philos(t_main *bag);
@@ -107,42 +107,42 @@ void		create_list(t_main *bag);
 void		assign_forks(t_main *bag);
 
 /*----- Dinner funcs -----*/
-void	start_dinner(t_main *bag);
+void		start_dinner(t_main *bag);
 
 /*----- Arbitrator -----*/
-void	stop_dinner(t_main *bag);
-int	is_philo_dead(t_philo *philo, int time);
-int	is_philo_full(t_philo *philo, int n_eat);
-int	all_philos_full(t_main *bag, int n_eat);
+void		stop_dinner(t_main *bag);
+int			is_philo_dead(t_philo *philo, int time);
+int			is_philo_full(t_philo *philo, int n_eat);
+int			all_philos_full(t_main *bag, int n_eat);
 
 /*----- Philo -----*/
-void	set_philo_state(t_philo *philo, int new_state);
-void	stop_eating(t_philo *philo);
-void	print_philo_status(t_philo *philo);
-void	increment_meals_counter(t_philo *philo);
-void	set_last_meal_time(t_philo *philo);
-int	get_philo_state(t_philo *philo);
-int	check_dinner_status(t_main *bag);
-int	loop_helper(t_philo *philo);
-int	philo_sleep(t_philo *philo);
-int	philo_take_fork(t_philo *philo, int which);
-int	philo_eat(t_philo *philo);
+void		set_philo_state(t_philo *philo, int new_state);
+void		stop_eating(t_philo *philo);
+void		print_philo_status(t_philo *philo);
+void		increment_meals_counter(t_philo *philo);
+void		set_last_meal_time(t_philo *philo);
+int			get_philo_state(t_philo *philo);
+int			check_dinner_status(t_main *bag);
+int			loop_helper(t_philo *philo);
+int			philo_sleep(t_philo *philo);
+int			philo_take_fork(t_philo *philo, int which);
+int			philo_eat(t_philo *philo);
 
 /*----- Free functions -----*/
-void	free_philo(t_philo *philo);
-void	free_list(t_main *bag);
+void		free_philo(t_philo *philo);
+void		free_list(t_main *bag);
 
 /*----- Mutex functions -----*/
-void	init_mutexes(t_main *bag);
-void	destroy_mutexes(t_main *bag);
+void		init_mutexes(t_main *bag);
+void		destroy_mutexes(t_main *bag);
 
 /*----- Utils -----*/
-void	ft_putstr_fd(char *s, int fd);
-void	ft_puterror(int error);
-void	start_forks(t_main *bag); // mutex init
-void	finish_forks(t_main *bag); // mutex destroy
-void	ft_usleep(long ms);
-long	get_time(void);
-long	ft_atol(const char *nptr);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_puterror(int error);
+void		start_forks(t_main *bag); // mutex init
+void		finish_forks(t_main *bag); // mutex destroy
+void		ft_usleep(long ms);
+long		get_time(void);
+long		ft_atol(const char *nptr);
 
 #endif
